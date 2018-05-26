d727f709b78b76156f002eebed07aaf2aad5bfe8
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-05-26 09:34:34 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
       item_name = req.path.split("/item/").last
       item = @@items.find {|i| i.name == item_name}
 
-      if !item.nil?
+      if @@items.include?(item)
         resp.write item.price
       else
         resp.write "Item not found"
