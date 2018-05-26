5a0674f8ebd2a9efc318020cc8d724394278215e
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-05-26 09:28:19 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
       item_name = req.path.split("/item/").last
       item = @@items.find {|i| i.name == item_name}
 
-      resp.write item.price  
+      resp.write item.price
     else
       resp.write "Page Not Found"
       resp.status = 404
